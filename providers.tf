e83c1f02a960519cbc96f5dabdbd99ce069729cd
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-16 16:25:00 -0500

    Updated the Providers Config

--- providers.tf
+++ providers.tf
@@ -1,18 +1,13 @@
-#Azure Provider source and version being used
-##
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0.0"
+      source = "hashicorp/azurerm"
+      version = "4.13.0"
     }
   }
-  backend "azurerm" {
-  }   
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
