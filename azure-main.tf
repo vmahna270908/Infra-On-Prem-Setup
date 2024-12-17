1b317c99a2a8f56f6fed811003ff13b0ccbcb6ac
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-17 11:13:54 -0500

    Deploy the Windows VM

--- azure-main.tf
+++ azure-main.tf
@@ -135,29 +135,7 @@ resource "azurerm_windows_virtual_machine" "windows_vm_domaincontroller" {
   }
 }
 
-locals { 
-  import_command       = "Import-Module ADDSDeployment"
-  password_command     = "$password = ConvertTo-SecureString ${var.domainpassword} -AsPlainText -Force"
-  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
-  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
-  shutdown_command     = "shutdown -r -t 10"
-  exit_code_hack       = "exit 0"
-  powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
-}
 
-resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
-  name                 = "create-active-directory-forest"
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
-  virtual_machine_id = azurerm_windows_virtual_machine.windows_vm_domaincontroller.id
-
-  settings = <<SETTINGS
-    {
-        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
-    }
-SETTINGS
-}
 
 
 
