d5d17d5768a588fe16b604635df9f073356eee98
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-17 12:47:48 -0500

    Clearing the DC Code

--- azure-main.tf
+++ azure-main.tf
@@ -107,61 +107,6 @@ resource "azurerm_subnet_network_security_group_association" "dc_subnet_nsg_asso
 
 }
 
-#VM for the DC
-resource "azurerm_windows_virtual_machine" "WIN_DC" {
-  name  = var.AZ-DC1
-  resource_group_name = data.azurerm_resource_group.Prod-RG.name
-  location            = data.azurerm_resource_group.Prod-RG.location
-  size                = "Standard_F2"
-  admin_username        = var.domainusername
-  admin_password        = var.domainpassword
-  network_interface_ids = [
-    azurerm_network_interface.dc_nic.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-  tags = {
-  type = "VM"
-  RG ="Prod-RG"
-  project = "Infra"
-  }
-}
-
-locals { 
-  import_command       = "Import-Module ADDSDeployment"
-  password_command     = "$password = ConvertTo-SecureString ${ var.domainpassword} -AsPlainText -Force"
-  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
-  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
-  shutdown_command     = "shutdown -r -t 10"
-  exit_code_hack       = "exit 0"
-  powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
-}
-
-resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
-  name                 = "create-active-directory-forest"
-  virtual_machine_id   = azurerm_windows_virtual_machine.WIN_DC.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
-
-  settings = <<SETTINGS
-    {
-        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
-    }
-SETTINGS
-}#
-
-
 
 
 
