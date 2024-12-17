fa96f8731eadfde1547b66c9e4b1a6aca9010fde
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-16 23:45:28 -0500

    Create and Attach the DC NSG

--- azure-main.tf
+++ azure-main.tf
@@ -74,16 +74,5 @@ resource "azurerm_subnet_network_security_group_association" "DC_Subnet_NSG_Asso
 
 }
 
-#Network Interface for the DC
-resource "azurerm_network_interface" "DC1-NIC" {
-  name                = "AZ-DC1-NIC"
-  location            = data.azurerm_resource_group.Prod-RG.location
-  resource_group_name = data.azurerm_resource_group.Prod-RG.name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.DC_SubNet.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = cidrhost(var.node_address_prefix_dc, 4)
-  }
-}
+
 
