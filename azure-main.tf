7381d91998f27ed7e567612d44957928658a6589
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-17 12:40:42 -0500

    Deploy the Windows VM and Promote to DC

--- azure-main.tf
+++ azure-main.tf
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "App_SubNet" {
 
 # Public IP for the DC
 resource "azurerm_public_ip" "dc_public_ip" {
-  name = var.AZ-DC1
+  name = "AZ-DC1-Public-IP"
   location            = data.azurerm_resource_group.Prod-RG.location
   resource_group_name = data.azurerm_resource_group.Prod-RG.name
   allocation_method   = "Dynamic"
@@ -108,7 +108,7 @@ resource "azurerm_subnet_network_security_group_association" "dc_subnet_nsg_asso
 }
 
 #VM for the DC
-resource "azurerm_windows_virtual_machine" "windows_vm_domaincontroller" {
+resource "azurerm_windows_virtual_machine" "WIN_DC" {
   name  = var.AZ-DC1
   resource_group_name = data.azurerm_resource_group.Prod-RG.name
   location            = data.azurerm_resource_group.Prod-RG.location
@@ -135,6 +135,30 @@ resource "azurerm_windows_virtual_machine" "windows_vm_domaincontroller" {
   RG ="Prod-RG"
   project = "Infra"
   }
+}
+
+locals { 
+  import_command       = "Import-Module ADDSDeployment"
+  password_command     = "$password = ConvertTo-SecureString ${ var.domainpassword} -AsPlainText -Force"
+  install_ad_command   = "Add-WindowsFeature -name ad-domain-services -IncludeManagementTools"
+  configure_ad_command = "Install-ADDSForest -CreateDnsDelegation:$false -DomainMode Win2012R2 -DomainName ${var.active_directory_domain} -DomainNetbiosName ${var.active_directory_netbios_name} -ForestMode Win2012R2 -InstallDns:$true -SafeModeAdministratorPassword $password -Force:$true"
+  shutdown_command     = "shutdown -r -t 10"
+  exit_code_hack       = "exit 0"
+  powershell_command   = "${local.import_command}; ${local.password_command}; ${local.install_ad_command}; ${local.configure_ad_command}; ${local.shutdown_command}; ${local.exit_code_hack}"
+}
+
+resource "azurerm_virtual_machine_extension" "create-active-directory-forest" {
+  name                 = "create-active-directory-forest"
+  virtual_machine_id   = azurerm_windows_virtual_machine.WIN_DC.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell.exe -Command \"${local.powershell_command}\""
+    }
+SETTINGS
 }
 
 
